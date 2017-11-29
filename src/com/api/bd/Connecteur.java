7370e2c8d86f8c87ab2f0e7fa3c81322bf58286c
chovCodeur <michel@lesecuriesdeventadour.com>
2017-11-29 01:06:26 +0100

    Externalisation des propriétés

--- src/com/api/bd/Connecteur.java
+++ src/com/api/bd/Connecteur.java
@@ -3,27 +3,28 @@ package com.api.bd;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
 
+
 public final class Connecteur {
-	
-    private final static String URL = "jdbc:mysql://127.0.0.1:3306/alchimist?zeroDateTimeBehavior=convertToNull";
+
+	private static ResourceBundle applicationProperties = ResourceBundle.getBundle("application");
+
+    //private final static String URL = "jdbc:mysql://127.0.0.1:3306/alchimist?zeroDateTimeBehavior=convertToNull";
     //private final static String URL = "jdbc:mysql://193.190.248.155:3306/alchimist";
 
-	private final static String LOGIN = "api";
-    private final static String PASSWORD = "api";
 	final static Logger logger = Logger.getLogger(Connecteur.class.getName());
 
     public static Connection getConnexion() throws SQLException {
-    	logger.debug("MIPA getConnexion sur "+ URL);
     		try {
 				Class.forName("com.mysql.jdbc.Driver");
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			}
 
-        final Connection con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            final Connection con = DriverManager.getConnection(applicationProperties.getString("bd.connect.url"), applicationProperties.getString("bd.connect.user"), applicationProperties.getString("bd.connect.user"));
         return con;
     }
 }
